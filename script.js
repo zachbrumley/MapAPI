31e1fc751bf53090b38d9fadb4765e8fcd961eb4
zachbrumley <zach.brumley@mscoding.org>
2022-05-10 13:51:13 -0500

    working on adding query search

--- script.js
+++ script.js
@@ -9,40 +9,50 @@ function GetMap() {
  function GeoLocate() {
  //get users current location
     if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(GetUserLoc);
+       navigator.geolocation.getCurrentPosition(GetUserLocation);
     }
  }
  
- function GetUserLoc(position) {
-     //set the devices location to the lat and lon variable
+ function GetUserLocation(position) {
      lat = position.coords.latitude;
      lon = position.coords.longitude;   
  
-     UpdateMapUserLoc();
+     UpdateUserLocation();
  }
  
- function UpdateMapUserLoc() {
-   
-     //on the map instance use the setView method to change the view of the map based on given settings
+ function UpdateUserLocation() {
      map.setView({
-         //specify the type of map style that should be displayed (aerial)
+         
          mapTypeID: Microsoft.Maps.MapTypeId.aerial,
-         //set center of the map to given location (devices current location)
+         
          center: new Microsoft.Maps.Location(lat, lon),
-         //controls how zoomed in the map will be
+         
          zoom: 16,   
      });
- 
-     //returns the location of the center of the current map view (in the setView method)
+
      var center = map.getCenter();
- 
-     //access the pushpin class and pass in the center of the map view as arg (so pin is set to that location)
+
          pin = new Microsoft.Maps.Pushpin(center, {
          title: 'You are here',
          color: 'green',
          enableHoverStyle: true
      });
  
-     //Add the pushpin to the map
      map.entities.push(pin);
- }//end function
+}
+
+    function SearchByType(poiSearchValue){
+        var select = document.getElementById("PointsOfInterest").value;
+        //var value = select.options[select.selectedIndex.value];
+
+        fetch("https://dev.virtualearth.net/REST/v1/LocalSearch/?query=" + {select} + "&userLocation=" + lat + "," + lon +"&key=AhIiBnir2ooVR3-ozNIk7kd-NCiAKE0M7mBByPngxrELAl4e2kfZx200kR0JdWRK")
+        .then(response => response.json())
+        .then(data => console.log(data)); 
+
+
+
+
+
+
+
+    }
